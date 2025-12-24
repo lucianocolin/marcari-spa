3f614cdc70bcd36113b5442bdc8c0c66806f09f8
lucianocolin <lucianocolin99@gmail.com>
2025-12-24 20:21:47 -0300

    Add contact page

--- src/pages/Contacto.tsx
+++ src/pages/Contacto.tsx
@@ -1,7 +1,20 @@
+import marcariLogo from "../assets/marcari-logo.jpeg";
+import "../css/Contacto.css";
+
 export default function Contacto() {
   return (
-    <div>
-      <h1>Contacto</h1>
+    <div className="contacto">
+      <img
+        src={marcariLogo}
+        alt="MARCARI-LOGO"
+        className="contacto-marcari-logo"
+      />
+
+      <p className="contacto-text">
+        CA MARCARI ESPAÑA 2017 S.L. <br /> <br />
+        Ramon de Moncada 14 – 3F <br /> <br />
+        (07181) Calviá – Is. Baleares – ESPAÑA
+      </p>
     </div>
   );
 }
